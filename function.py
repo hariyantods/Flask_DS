b2f7fdeb4cf8a8442e826a620a85a7d88c361b5d
Hariyanto <hariyantoliem@Hariyantos-MacBook-Pro.local>
2022-11-09 16:36:54 +0700

    tambah fungsi

---
+++ function.py
@@ -0,0 +1,10 @@
+def tambah(angka_1, angka_2):
+    """Fungsi menambah 2 angka"""
+    return angka_1 + angka_2
+
+print("masukkan angka1: ")
+angka_1 = float(input())
+angka_2 = float(input())
+
+hasil = tambah(angka_1, angka_2)
+print(f"Hasilnya: {hasil}")
